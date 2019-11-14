1efa52ed2301f753332fa6b9199a1c952d0492c6
LeenaYaseen <leena4.y@gmail.com>
2019-11-14 20:17:37 +0300

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,50 +1,86 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 /*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";*/
 
 
-var cards = ['queen','queen','king','king'];
+var cards = 
+[
+	{	
+		rank: "queen",
+		suit: "hearts",
+		cardImage:"images/queen-of-hearts.png",
+	
+	},
+
+	{	
+		rank: "queen",
+		suit: "diamonds",
+		cardImage:"images/queen-of-diamonds.png",
+	
+	},
+
+	{	
+		rank: "king",
+		suit: "hearts",
+		cardImage:"images/king-of-hearts.png",
+	
+	},
+
+	{	
+		rank: "king",
+		suit: "diamonds",
+		cardImage:"images/king-of-diamonds.png",
+	
+	},	
+];
+
 var cardsInPlay = [];
 
-flipCard(0);
-flipCard(2);
 
 function checkForMatch()
 {
 
 	if (cardsInPlay[0] === cardsInPlay[1])
 	 	{
-  			console.log("You found a match!");
+  			alert("You found a match!");
   		}
 
 	else 
 		{
-  			console.log("Sorry, try again.");
+  			alert("Sorry, try again.");
 		}
 }
 
-function flipCard(cardId)
-{
-    console.log("User flipped" + " " + cards[cardId])
 
-	cardsInPlay.push(cards[cardId]);
+function createBoard()
+{
+	for(var i = 0; i < cards.length; i++)
+	{
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+
+}
+
+function flipCard()
+{
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage);
+    console.log("User flipped" + " " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2)
 	{
-
 		checkForMatch();
-
-		if (cardsInPlay[0] === cardsInPlay)
- 		{
-
- 			//alert("you found a match!");
- 		}
- 		else
- 		{
- 			//alert("Sorry, try again.");
- 		}
  	}
 }
+createBoard();
+
 
